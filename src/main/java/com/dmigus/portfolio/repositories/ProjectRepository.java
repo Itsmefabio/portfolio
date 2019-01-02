6bfb74d03542daf5cf686ea88681df2c4998fa59
Damian Migus <damian.migus@gmail.com>
2019-01-02 12:22:45 +0100

    changed properties to make connection with client api and db, changed version of mysql connector, added driver classname property

--- src/main/java/com/dmigus/portfolio/repositories/ProjectRepository.java
+++ src/main/java/com/dmigus/portfolio/repositories/ProjectRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.dmigus.portfolio.models.Project;
+
 @RepositoryRestResource
 @CrossOrigin(origins = "http://localhost:4200")
 public interface ProjectRepository extends JpaRepository<Project,Integer> {
